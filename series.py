ea6ea16682df1c3c0fff897a39b57eefbdbdb1c0
Falashade Greene <falashadegreene@gmail.com>
2022-07-20 18:33:06 -0400

    added string and resource definition for fibonacci and lucas sequence

--- series.py
+++ series.py
@@ -1,4 +1,10 @@
 def fibonacci(n):
+    """
+    The Fibonacci sequence is a type series where each number is the sum of the two that precede it.
+    It starts from 0 and 1 usually.
+    The Fibonacci sequence is given by 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, and so on.
+     - https://byjus.com/maths/fibonacci-sequence/
+    """
     if n <= 1:
         return n
     return fibonacci(n - 1) + fibonacci(n - 2)
@@ -9,6 +15,12 @@ def fibonacci(n):
 
 
 def lucas(n):
+    """
+    The Lucas series has the same recursive relationship as the Fibonacci sequence, where each
+    term is the sum of the two previous terms, but with different starting values.
+    https://en.wikipedia.org/wiki/Lucas_number
+    """
+
     if n == 0:
         return 2
     if n == 1:
@@ -33,6 +45,6 @@ def sum_series(n):
 
 if __name__ == '__main__':
     n = int(input('> '))
-    print(f'The {n} th  fibonacci number  is {fibonacci(int(n))}, th lucas number is {int((n))} the sum of those two is {sum_series(int(n))}')
+    print(f'The {n} th  fibonacci number  is {fibonacci(int(n))}, th lucas number is {lucas(int(n))} the sum of those two is {sum_series(int(n))}')
 
 
